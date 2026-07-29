a8d8c020895b010d14b5b6860a7d5cd65ea7c142
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-29 18:42:24 +0530

    Organize C programs into topic-wise folders

---
+++ Arrays/notesofarrays.c
@@ -0,0 +1,21 @@
+/*
+
+syntex and declaration 
+int arr[5] = {2,3,5,7,9};
+
+
+indices = 0 1 2 3 4  // for arry of 5 the index start from 0 to 4 
+array   = 2 3 5 7 9
+
+         arr[2] is the index of 5 
+         arr[0] is the index of 2 
+         arr[4] is the index of 9 
+this is the way in which we can point that the which array is for whose index
+ and we can print that value  
+
+
+ **int stores 4 bytes 
+ and in array memory allogation is countiounius
+
+ ***address of array elements 
+ is names or  denoted identify by first  element of array 
