7a2a839e774c5ca73446088795b649914d4a763e
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-29 18:44:30 +0530

    Organize C programs into topic-wise folders

---
+++ Arrays/o3inilizationtechnique.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int arr[5];
+    arr[0]=1;
+    arr[1]=4;
+    arr[2]=7;
+    arr[3]=90;
+    arr[4]=30;
+    arr[4]=50;//updatation
+    printf("%d",arr[4]);
+    return 0;
+}
