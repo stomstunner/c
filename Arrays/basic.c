0bc45944b0d9a8007dd00183a59503392e1353ea
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-29 18:41:58 +0530

    Organize C programs into topic-wise folders

---
+++ Arrays/basic.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int arr[5] = {4,5,6,7,8}; // 5 dabbe creat kar liye array of index start from 0,1,2 3,4 
+    //whereas 0 is the index of 4, and 1 is the index asign for  5
+    printf("%d\n",arr[2]); // it prints the value that is in the arr[2]=6
+    printf("%d",arr[1]);
+    return 0;
+}
