552d424e99dddd378e096e60e323356ee0803b95
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-29 18:44:41 +0530

    Organize C programs into topic-wise folders

---
+++ Arrays/o11adresses.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int arr[5] ={ 1, 3 , 5, 6, 7};
+    printf("%p\n",&arr[0]);
+    printf("%p\n",&arr[1]);
+    printf("%p\n",&arr[2]);
+    printf("%p\n",&arr[3]);
+    printf("%p\n",&arr[4]);
+    return 0;
+}
