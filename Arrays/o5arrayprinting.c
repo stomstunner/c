d0c004ea541d79db9219e03cddbb62f16c6cdd87
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-29 18:43:09 +0530

    Organize C programs into topic-wise folders

---
+++ Arrays/o5arrayprinting.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};  // here we asign our inputs ,,[5] inputs
+    for (int i = 0; i <= 4; i++)  // for printing 5 elements the for loop runs to 0 to 4
+    {
+        printf("%d\n", arr[i]); // this code prints the whole array,, by the help of for loop
+    }
+    return 0;
+}
