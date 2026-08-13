d00ccf082a758fdb15414249219e9096b1f0e5e9
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-13 18:37:34 +0530

    previous c  language code commit

---
+++ basic/AreaOfCircleINPUT.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float radius ;
+    printf("Enter radius : ");
+    scanf("%f",&radius);
+    float area = 22 * radius * radius / 7;
+    printf("The Area Of Circle Is %f",area);
+    return 0;
+}
