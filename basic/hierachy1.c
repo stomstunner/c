38f2db54d5ba2ee4de3c298d020cde7bac38f12b
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-13 18:39:40 +0530

    previous c  language code commit

---
+++ basic/hierachy1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i = 2, j = 3, k, l;
+    float a, b;
+    k =i/j*k;
+    l =j/i*i;
+    a =i/j*k;
+    b =j/i*i;
+    printf(" %d\n %d\n %f\n %f",k, l, a, b);
+    return 0;
+}
