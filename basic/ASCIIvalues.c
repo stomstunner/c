c851f50e30d5785bb412cf9e9eeda41051852355
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-13 18:38:33 +0530

    previous c  language code commit

---
+++ basic/ASCIIvalues.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char ch = 'A';
+    printf("%d\n",ch);//the ASCII values of A is 65 ...90
+    char ph = 'a';
+    printf("%d\n",ph);//the ASCII values of a is 97... 122
+    char py = '@';
+    printf("%d  ",py);
+    return 0;
+}
