7833d39e72c30c80a9859e80c99977924efab61d
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-13 18:38:23 +0530

    previous c  language code commit

---
+++ basic/firstprogram.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf("hello world\n");
+    printf("hello ujjwal");
+    printf("\nhello ujjwal");
+    printf("nn\n\nnn\n");
+    printf("n/nn/nn");
+   
+
+    return 0;
+}
