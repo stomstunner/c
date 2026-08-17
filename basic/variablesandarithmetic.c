6dea3aa1e6f204299b9b4ffd912666d8f699b7d1
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-17 20:27:18 +0530

    commit to previous  c programming codes

---
+++ basic/variablesandarithmetic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+
+
+printf("ARITHMETIC OPERATIONS ON int DATA TYPE");        
+
+int x = 5;
+int y = 2;
+
+printf("\n%d",x+y);
+printf("\n%d",x-y);
+printf("\n%d",x*y);
+printf("\n%d",x/y);
+return 0;
+}
