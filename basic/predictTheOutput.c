e1a394d6dcfd523319cbc9ecc0c596b64bbf1985
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-17 20:26:05 +0530

    commit to previous  c programming codes

---
+++ basic/predictTheOutput.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int p,q,r,s;
+    printf("Enter values of p, q, r and s respectively ");
+    scanf("%d%d%d%d",&p,&q,&r,&s);
+    printf("p = %d q = %d r = %d s = %d",p,q,r,s);
+    return 0;
+}
