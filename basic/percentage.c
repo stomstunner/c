9c5155c03a5cc84c45269d002b088f7cff9a8f15
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-17 20:25:20 +0530

    commit to previous  c programming codes

---
+++ basic/percentage.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    float m1 = 72; // maths marks
+    float m2 = 89; // scirnce marks
+    float m3 = 94; // hindi marks
+    float m4 = 93; // english marks
+    float m5 = 96; // social science marks
+    float p = (m1 + m2 + m3 + m4 + m5) / 5;
+    printf("Percentage:%f", p);
+
+    return 0;
+}
