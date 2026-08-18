731086f5d04e5a40b730e2c78f2158d41af3113e
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-08-18 17:16:21 +0530

    commit to previous  c programming codes

---
+++ COLLEGE/insertion1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int arr[20];
+    int x = 5;
+    int pos = 27;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i = n-1;i>=pos-1;i--){
+        arr[i+1] arr[i];
+    }
+}
